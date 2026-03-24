c583563b925bca0f230d8430f47efb178f05f932
FGguy <y.pilon9@gmail.com>
2026-03-24 17:43:38 -0400

    use wsl

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ mod server;
 
 fn main() {
     let args = cli::Args::parse();
-    let ip_addr = format!("127.0.0.1:{}", args.port);
+    let ip_addr = format!("0.0.0.0:{}", args.port);
 
     let socket = UdpSocket::bind(&ip_addr)
         .unwrap_or_else(|e| panic!("Failed to bind UDP socket to: {}, error: {}", &ip_addr, e));
